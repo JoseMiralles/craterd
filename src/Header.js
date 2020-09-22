8cc73a3d592a88a917ae2705706abbd3c503ef68
Jose <josemiralles1991@gmail.com>
2020-09-22 19:37:28 -0400

    fixed nav bar

--- src/Header.js
+++ src/Header.js
@@ -6,37 +6,40 @@ class Header extends React.Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         {/* Branding */}
-        <a className="navbar-brand mx-lg-5 mx-2" href="#">
+        <a className="navbar-brand ml-lg-5" href="#">
           CRATERD
         </a>
 
-{/* Mobile collapsible button */}
-<button
-  className="navbar-toggler"
-  type="button"
-  data-toggle="collapse"
-  data-target="#navbarSupportedContent"
-  aria-controls="navbarSupportedContent"
-  aria-expanded="false"
-  aria-label="Toggle navigation">
-  <span className="navbar-toggler-icon"></span>
-</button>
+        {/* Mobile collapsible button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
         {/* Navbar collapsable section */}
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-        {/* Search */}
-        <form className="form-inline my-2 mx-lg-3 m-0">
-          <input
-            className="form-control"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-light my-2 my-sm-0" type="submit">
-            -&gt;
-          </button>
-        </form>
+          {/* Search */}
+          <form className="form-inline my-2 mx-lg-3 m-0">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-light my-2 my-sm-0"
+              type="submit"
+            >
+                <i className="material-icons">search</i>
+            </button>
+          </form>
 
           {/* Links */}
           <ul className="navbar-nav ml-auto mr-lg-3">
@@ -62,7 +65,10 @@ class Header extends React.Component {
               >
                 UserName
               </a>
-              <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <div
+                className="dropdown-menu dropdown-menu-right"
+                aria-labelledby="navbarDropdown"
+              >
                 <a className="dropdown-item" href="#">
                   Profile
                 </a>
@@ -77,7 +83,6 @@ class Header extends React.Component {
             </li>
           </ul>
         </div>
-
       </nav>
     );
   }
