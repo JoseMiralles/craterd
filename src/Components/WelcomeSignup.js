64925b4a15f5d3ed27507992a0b6652b1776123f
Jose <josemiralles1991@gmail.com>
2020-09-23 20:32:20 -0400

    Removed the margin at the top of the signup banner for screens smaller than lg

--- src/Components/WelcomeSignup.js
+++ src/Components/WelcomeSignup.js
@@ -4,11 +4,11 @@ class WelcomeSignup extends React.Component {
   render() {
     return (
         <div className="container-fluid welcome-signup-box m-0 text-light">
-          <div className="container mt-md-4 mb-3 p-5">
+          <div className="container mt-lg-4 mb-3 p-5">
           <div className="row">
             <div className="col-md-7 p-md-5 mb-2">
               <h1>Welcome</h1>
-              <p>We are the number one providers of wholesale raw asteroid ore and asteroid refined metals in all 9 sectors!</p>
+              <p>We are the number one providers of wholesale raw asteroid ore and asteroid refined metals in all five sectors!</p>
               <button className="btn btn-outline-light px-4" type="submit">
                 Sign Up
               </button>
